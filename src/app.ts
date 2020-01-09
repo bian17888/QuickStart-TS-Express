d21fbcfda42b265c771e67ee434b351894ffcab9
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2020-01-09 17:43:52 +0800

    add static pages

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,20 @@
+import path from 'path';
 import express from 'express';
+
+import homeController from './controllers/home';
 
 // Create Express server
 const app = express();
+
+// Express configuration
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'pug');
+
+/**
+ * Primary app routes.
+ */
+app.get("/", homeController);
+
 
 export default app;
---
+++ src/controllers/home.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+
+/**
+ * GET /
+ * Home page.
+ */
+const index = (req: Request, res: Response) => {
+    res.render('home', { title: 'Home Page', message: 'hello home page' });
+}
+
+export default index;
