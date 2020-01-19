9950ce5a05f2ae35c079fc4cd2755ce874362992
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2020-01-19 12:04:56 +0800

    update : static resource

--- src/app.ts
+++ src/app.ts
@@ -11,10 +11,11 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
 
+app.use('/static', express.static(path.join(__dirname, '../static')));
+
 /**
  * Primary app routes.
  */
 app.get("/", homeController);
-
 
 export default app;
