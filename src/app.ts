ba151ccb219541bf8bc0b585bd60e0fa30329453
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2020-02-14 16:04:11 +0800

    add some comments

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
+/**
+ * 1 Import Dependencies
+ */
 import path from 'path';
 import express from 'express';
 import morgan from 'morgan';
@@ -20,31 +23,38 @@ const config = {
         encrypt: true
     }
 }
-
-// Create Express server
 const app = express();
 
-// Connect Database
-(async function () {
-    try {
-        const pool = sql.connect(config);
-    } catch (err) {
 
-    }
-})();
-
-
-
-// Express configuration
+/**
+ * 2 Express configuration
+ */
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
 
+
+/**
+ * 3 Connect Database
+ */
+(async function () {
+    try {
+        const pool = sql.connect(config);
+    } catch (err) {
+        console.log(err);
+    }
+})();
+
+
+/**
+ * 4 Use Middleware
+ */
 app.use('/static', express.static(path.join(__dirname, '../static')));
 app.use(morgan('tiny'));
 
+
 /**
- * Primary app routes.
+ * 5 Set Routes.
  */
 app.use('/books', bookRouter());
 app.get("/", homeController);
