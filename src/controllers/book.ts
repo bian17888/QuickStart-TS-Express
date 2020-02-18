e2254329aae88822568d9e28d6a517d778e89e13
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2020-02-18 18:53:10 +0800

    to use mongodb instead of  mssql

--- src/controllers/book.ts
+++ src/controllers/book.ts
@@ -1,29 +1,47 @@
 import { Request, Response } from 'express'
+import { MongoClient, ObjectID } from 'mongodb';
 import sql from 'mssql';
-import debug from 'debug'
+import debug from 'debug';
+
+const debugBook = debug('app:bookController');
+
+// The config of mongodb
+const url = 'mongodb://localhost:27017';
+const dbName = 'express-demo';
+const colName = 'books';
 
 async function index(req: Request, res: Response) {
-    const request = new sql.Request();
+    let client;
     try {
-        const { recordset } = await request.query('select * from books');
-        debug('app:bookController')(recordset);
+        // Connect using MongoClient
+        client = await MongoClient.connect(url);
+        const col = client.db(dbName).collection(colName);
+
+        const result = await col.find({}).toArray();
+        debugBook(result);
         res.render('book/list', {
-            data: recordset
+            data: result
         })
     } catch (err) {
 
     }
+    client.close();
 }
 
 async function detail(req: Request, res: Response) {
     const { id } = req.params;
-    const request = new sql.Request();
+    const _id = new ObjectID(id);
+    let client;
 
     try {
-        const { recordset } = await request.input('id', sql.Int, id).query('SELECT TOP (1) * FROM books WHERE id = @id');
-        debug('app:bookController')(recordset);
+        // Connect using MongoClient
+        client = await MongoClient.connect(url);
+        const col = client.db(dbName).collection(colName);
+
+        const result = await col.findOne({ _id });
+        debugBook(result);
         res.render('book/detail', {
-            data: recordset[0]
+            data: result
         })
     } catch (err) {
         debug(err);
