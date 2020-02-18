2ce194bc9b46c3d780000ca48c36d63ea1aeb31a
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2020-02-18 14:19:01 +0800

    add seed.js

---
+++ docs/seed.js
@@ -0,0 +1,12 @@
+const books = [{
+    title: 'War and Peace',
+    author: 'Lev Nikolayevich Tolstoy'
+},
+{
+    title: 'Les Misérables',
+    author: 'Victor Hugo'
+},
+{
+    title: 'The Time Machine',
+    author: 'H. G. Wells'
+}];
